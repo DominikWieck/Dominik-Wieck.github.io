42f56b2e038930a2c2755de11a38a653b23841a9
Dominik <56441910+DominikWieck@users.noreply.github.com>
2019-12-02 20:40:00 +0100

    Add files via upload

---
+++ Aufgabe_7_Praktikum_29_11_19/drumpad.js
@@ -0,0 +1,90 @@
+//Kick
+window.addEventListener("load", function () {
+    document.querySelector("#button1").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/kick.mp3");
+        sound.play();
+    }
+});
+//Snare
+window.addEventListener("load", function () {
+    document.querySelector("#button2").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/snare.mp3");
+        sound.play();
+    }
+});
+//High-Hat
+window.addEventListener("load", function () {
+    document.querySelector("#button3").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/hihat.mp3");
+        sound.play();
+    }
+});
+//Tone F
+window.addEventListener("load", function () {
+    document.querySelector("#button4").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/F.mp3");
+        sound.play();
+    }
+});
+//Tone G
+window.addEventListener("load", function () {
+    document.querySelector("#button5").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/G.mp3");
+        sound.play();
+    }
+});
+//Tone A
+window.addEventListener("load", function () {
+    document.querySelector("#button6").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/A.mp3");
+        sound.play();
+    }
+});
+//Tone C
+window.addEventListener("load", function () {
+    document.querySelector("#button7").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/C.mp3");
+        sound.play();
+    }
+});
+//Laugh, well actually its Chewbacca
+window.addEventListener("load", function () {
+    document.querySelector("#button8").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/Chewbacca_Sound_6.mp3");
+        sound.play();
+    }
+});
+//Laugh, well not even close bby
+window.addEventListener("load", function () {
+    document.querySelector("#button9").addEventListener("mousedown", playSample);
+    function playSample() {
+        var sound = new Audio("assets/not_even_close.mp3");
+        sound.play();
+    }
+});
+//Play that stuff
+window.addEventListener("load", function (Beat) {
+    document.querySelector("#playb").addEventListener("click", StartBeat);
+});
+function StartBeat() {
+    var inter = setInterval(Beat, 300);
+    var myBeatyo = ["assets/kick.mp3", "assets/kick.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/not_even_close.mp3"];
+    var index = 0;
+    function Beat() {
+        var MyMelody = new Audio(myBeatyo[index]);
+        MyMelody.play();
+        index += 1;
+        if (index > 11)
+            index = 0;
+        console.log(myBeatyo[index]);
+    }
+}
+//# sourceMappingURL=drumpad.js.map
