41cb656012fe1e753542f9b9c064bde4284cf2dc
Dominik <56441910+DominikWieck@users.noreply.github.com>
2019-12-12 00:03:46 +0100

    Add files via upload

---
+++ Aufgabe_8_Praktikum_06_12_19_/drumpad.js
@@ -0,0 +1,55 @@
+var myBeat = ["kick.mp3", "kick.mp3", "snare.mp3", "hihat.mp3", "not_even_close.mp3"];
+var aufnahme = false;
+window.addEventListener("load", function () {
+    document.querySelector("#button1").addEventListener("mousedown", function () { playSample("kick.mp3"); });
+    document.querySelector("#button2").addEventListener("mousedown", function () { playSample("snare.mp3"); });
+    document.querySelector("#button3").addEventListener("mousedown", function () { playSample("hihat.mp3"); });
+    document.querySelector("#button4").addEventListener("mousedown", function () { playSample("A.mp3"); });
+    document.querySelector("#button5").addEventListener("mousedown", function () { playSample("C.mp3"); });
+    document.querySelector("#button6").addEventListener("mousedown", function () { playSample("F.mp3"); });
+    document.querySelector("#button7").addEventListener("mousedown", function () { playSample("G.mp3"); });
+    document.querySelector("#button8").addEventListener("mousedown", function () { playSample("Chewbacca_Sound_6.mp3"); });
+    document.querySelector("#button9").addEventListener("mousedown", function () { playSample("not_even_close.mp3"); });
+    document.querySelector("#play").addEventListener("click", playBeatz);
+    document.querySelector("#record").addEventListener("click", aufnehmen);
+    document.querySelector("#delete").addEventListener("click", loschen);
+});
+//Samples
+function playSample(myMelody) {
+    var audio = new Audio("assets/" + myMelody);
+    audio.play();
+    if (aufnahme == true) {
+        myBeat.push(myMelody);
+    }
+}
+//Beat Function
+var tempo;
+var index = 0;
+function playBeatz() {
+}
+//Beotyoo
+function beot() {
+    playSample(myBeat[index]);
+    index += 1;
+    console.log(index);
+    if (index > (myBeat.length - 1))
+        index = 0;
+}
+//Löschen
+function loschen() {
+    myBeat.length = 0;
+}
+//Aufnahme
+function aufnehmen() {
+    aufnahme = true;
+}
+//Button Pause/Play
+var meinButton = document.querySelector("#play");
+if (meinButton.className == "fas fa-play") {
+    meinButton.className = "fas fa-stop";
+    aufnahme = false;
+}
+else {
+    meinButton.className = "fas fa-play";
+}
+//# sourceMappingURL=drumpad.js.map
