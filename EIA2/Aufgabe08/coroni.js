474f29d47402b198ace66a0cfd6816e79cb97b44
Dominik <56441910+DominikWieck@users.noreply.github.com>
2020-06-12 16:19:29 +0200

    Add files via upload

---
+++ EIA2/Aufgabe08/coroni.js
@@ -0,0 +1,92 @@
+"use strict";
+var L08_Coroni;
+(function (L08_Coroni) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+    let golden = 0.62;
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
+        drawBackground();
+        drawCoroni({ x: 10, y: 10 }, { x: 250, y: 250 });
+        drawBloodcells({ x: 30, y: 40 }, { x: 250, y: 250 });
+    }
+    function drawBackground() {
+        console.log("Background");
+        let pattern = document.createElement("canvas").getContext('2d');
+        pattern.canvas.width = 40;
+        pattern.canvas.height = 20;
+        pattern.fillStyle = '#db305e';
+        pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+        pattern.moveTo(0, 10);
+        pattern.lineTo(5, 10);
+        pattern.lineTo(20, 0);
+        pattern.lineTo(30, 0);
+        pattern.lineTo(40, 10);
+        pattern.lineTo(30, 20);
+        pattern.lineTo(20, 20);
+        pattern.lineTo(10, 10);
+        pattern.stroke();
+        crc2.fillStyle = crc2.createPattern(pattern.canvas, 'repeat');
+        crc2.fillRect(0, 0, canvas.width, canvas.height);
+        pattern.strokeStyle = "";
+        pattern.stroke();
+        pattern.closePath();
+        //Punkt in Zelle
+        pattern.beginPath();
+        pattern.arc(50, 20, 2, 0, 2 * Math.PI);
+        pattern.fillStyle = "#16ff0a";
+        pattern.fill();
+        crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawCoroni(_position, _size) {
+        console.log("Bloodcell", _position, _size);
+        let nParticles = 50;
+        let radiusParticle = 50;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "#4b8bbf");
+        //
+        gradient.addColorStop(1, "#000000");
+        //
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+        }
+        crc2.restore;
+    }
+    function drawBloodcells(_position, _size) {
+        console.log("Bloodcell", _position, _size);
+        let nParticles = 70;
+        let radiusParticle = 30;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "#a33636");
+        //
+        gradient.addColorStop(1, "#000000");
+        //
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+        }
+        crc2.restore;
+    }
+})(L08_Coroni || (L08_Coroni = {}));
+//# sourceMappingURL=coroni.js.map
