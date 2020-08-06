d4b95cedabade26888711074c3f2b7b38eeed051
Dominik <56441910+DominikWieck@users.noreply.github.com>
2020-08-06 18:47:29 +0200

    Delete Arti.js

--- EIA2/Lektion04/Arti.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var L04_HouseHold;
-(function (L04_HouseHold) {
-    let data, { Articles: [{ name: , "Butter": , price: , 20.00:  }, { name: , "Milk": , price: , 69.00:  }, { name: , "Eggs": , price: , 31.00:  }], Shop: [{ name: , "REWE":  }, { name: , "ALDI":  }, { name: , "LIDL":  }], Chores: [{ name: , "Cleaning": , price: , 6.00:  }, { name: , "Cooking": , price: , 8.00:  }, { name: , "Cleaning": , price: , 7.00:  }], Bank: [{ name: , "PayPal":  }, { name: , "Transfer":  }, { name: , "Cash":  }] };
-})(L04_HouseHold || (L04_HouseHold = {}));
-//# sourceMappingURL=Arti.js.map
